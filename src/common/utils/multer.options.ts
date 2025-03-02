7b6a0fdc45fd480e52c3504d81162e0718c2a728
jinhwan <song7022556@gmail.com>
2025-03-03 03:33:55 +0900

    feat:버킷이름 변경

--- src/common/utils/multer.options.ts
+++ src/common/utils/multer.options.ts
@@ -5,6 +5,8 @@ import path from 'path';
 import multerS3 from 'multer-s3';
 import dotenv from 'dotenv';
 dotenv.config();
+console.log('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID);
+console.log('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY);
 export const multerImage = () => {
   if (process.env.NODE_ENV === 'production') {
     // s3설정
