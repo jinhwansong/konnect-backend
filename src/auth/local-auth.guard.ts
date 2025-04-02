de4e63e976ee6d8c25147a399c1b0edea1c9d7bd
jinhwan <song7022556@gmail.com>
2025-04-03 01:36:33 +0900

    test:일반로그인시 문제 해결중2

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -5,20 +5,41 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth.service';
+import { ConfigService } from 'node_modules/@nestjs/config';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-  constructor(private readonly authService: AuthService) {
+  constructor(private configService: ConfigService) {
     super();
   }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const can = await super.canActivate(context);
       if (can) {
         const request = context.switchToHttp().getRequest();
-        // request.session.cookie.domain = '.konee.shop';
+        const response = context.switchToHttp().getResponse();
+
+        // 로그인 실행
         await super.logIn(request);
+
+        // 기존 쿠키 값 획득
+        const sessionCookie = request.cookies['connect.sid'];
+        if (sessionCookie) {
+          // 기존 쿠키를 제거
+          response.clearCookie('connect.sid');
+
+          // 새 쿠키 설정 (명시적으로 모든 옵션 지정)
+          const isProd = this.configService.get('NODE_ENV') === 'production';
+          response.cookie('connect.sid', sessionCookie, {
+            domain: isProd ? '.konee.shop' : 'localhost',
+            path: '/',
+            httpOnly: true,
+            secure: isProd,
+            maxAge: 3600000,
+            sameSite: isProd ? 'none' : 'lax',
+          });
+        }
       }
       return true;
     } catch (error) {
